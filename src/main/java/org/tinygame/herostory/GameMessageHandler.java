d3c763110f0be69131f865fda6a88fb352eec7f7
王越 <yue.wang@szctjt.cn>
2021-06-25 15:15:40 +0800

    集成log4j2

--- src/main/java/org/tinygame/herostory/GameMessageHandler.java
+++ src/main/java/org/tinygame/herostory/GameMessageHandler.java
@@ -1,16 +1,20 @@
-package org.tinygame;
+package org.tinygame.herostory;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * 自定义消息处理器
  */
 public class GameMessageHandler extends SimpleChannelInboundHandler<Object> {
+    static private final Logger logger = LogManager.getLogger();
+
     protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
-        System.out.println("收到客户端消息：" + msg);
+        logger.info("收到客户端消息：" + msg);
 
         BinaryWebSocketFrame frame = (BinaryWebSocketFrame) msg;
         ByteBuf byteBuf = frame.content();
